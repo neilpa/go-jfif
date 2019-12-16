888fd39ab1f1caa42c52322524c7a7649fc8a754
Neil Pankey <npankey@gmail.com>
2019-12-15 19:11:13 -0800

    jfifstat: Dump marker and payload length

---
+++ cmd/jfifstat/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"neilpa.me/go-jfif"
+)
+
+func main() {
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	segs, err := jfif.DecodeSegments(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, s := range segs {
+		fmt.Printf("%x %d\n", s.Marker, len(s.Data))
+	}
+}
