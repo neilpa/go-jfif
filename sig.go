eb15553fc3f20511d3a7054d9a7a752f51f02316
Neil Pankey <npankey@gmail.com>
2019-12-22 11:07:05 -0500

    jfif: Add more APPn sigs
    http://www.ozhiker.com/electronics/pjmt/jpeg_info/app_segments.html

--- sig.go
+++ sig.go
@@ -11,8 +11,25 @@ const (
 // APP1
 const (
 	SigExif        = "Exif\x00\x00"
+	SigExifAlt     = "Exif\x00\xFF"
 	SigXMP         = "http://ns.adobe.com/xap/1.0/\x00"
 	SigExtendedXMP = "http://ns.adobe.com/xmp/extension/\x00"
+)
+
+// APP2
+const (
+	SigICCProfile = "ICC_PROFILE\x00"
+)
+
+// APP3
+const (
+	SigMETA = "META\x00\x00"
+	SigMeta = "Meta\x00\x00"
+)
+
+// APP12
+const (
+	SigDucky = "Ducky\x00"
 )
 
 // APP13
@@ -21,9 +38,17 @@ const (
 	SigPhotoshop2 = "Adobe_Photoshop2.5:"
 )
 
+// APP14
+const (
+	SigAdobe = "Adobe\x00"
+)
+
+
 var appnSigs = [16][]string{
 	{SigJFIF, SigJFXX},
 	{SigExif, SigXMP, SigExtendedXMP},
+	{SigICCProfile},
+	{SigMETA, SigMeta},
 	{},
 	{},
 	{},
@@ -32,8 +57,7 @@ var appnSigs = [16][]string{
 	{},
 	{},
 	{},
-	{},
-	{},
-	{},
+	{SigDucky},
 	{SigPhotoshop3, SigPhotoshop2},
+	{SigAdobe},
 }
