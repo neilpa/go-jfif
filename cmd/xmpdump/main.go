5feecdbb176a4363a57063b6ce24631b41269202
Neil Pankey <npankey@gmail.com>
2019-12-22 11:57:26 -0500

    cmd/xmpdump: Naive merge of extended segments
    Adds a couple of example test images

--- cmd/xmpdump/main.go
+++ cmd/xmpdump/main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"log"
 	"os"
-	"strings"
 
 	"neilpa.me/go-jfif"
-)
-
-const (
-	sigXMP = "http://ns.adobe.com/xap/1.0/\x00"
-	sigExtendedXMP = "http://ns.adobe.com/xmp/extension/\x00"
 )
 
 func main() {
@@ -26,14 +22,38 @@ func main() {
 	}
 
 	for _, s := range segs {
-		if s.Marker == jfif.APP1 {
-			data := string(s.Data)
-			if strings.HasPrefix(data, sigXMP) {
-				fmt.Println(data)
-			} else if strings.HasPrefix(data, sigExtendedXMP) {
-				// TODO Merge data that spans multiple segments
-				fmt.Println(data)
+		sig, payload, _ := s.AppPayload()
+		switch sig {
+		case jfif.SigXMP:
+			fmt.Println(string(payload))
+		case jfif.SigExtendedXMP:
+			var h header
+			r := bytes.NewReader(payload)
+			err = binary.Read(r, binary.BigEndian, &h)
+			if err != nil {
+				log.Fatal(err)
 			}
+			// TODO Buffer if different extendedd XMP segments are
+			// interleaved or they are in the wrong order
+			fmt.Print(string(payload[binary.Size(h):]))
 		}
 	}
+}
+
+// Each chunk is written into the JPEG file within a separate APP1 marker
+// segment. Each ExtendedXMP marker segment contains:
+//
+//  A null-terminated signature string "http://ns.adobe.com/xmp/extension/".
+//  A 128-bit GUID stored as a 32-byte ASCII hex string, capital A-F, no
+//    null termination. The GUID is a 128-bit MD5 digest of the full
+//    ExtendedXMP serialization.
+//  The full length of the ExtendedXMP serialization as a 32-bit unsigned
+//    integer.
+//  The offset of this portion as a 32-bit unsigned integer.
+//  The portion of the ExtendedXMP.
+
+type header struct {
+	MD5Hash    [32]byte
+	FullLength uint32
+	Offset     uint32
 }
