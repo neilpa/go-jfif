12cf04b8b2feefa6a5d1ef18440b112db505fa1e
Neil Pankey <npankey@gmail.com>
2020-03-24 15:59:59 -0700

    jfifstat: Update arg handling

--- cmd/jfifstat/main.go
+++ cmd/jfifstat/main.go
@@ -1,26 +1,62 @@
+// jfifstat prints segment marker, size, and signature info from JPEG files.
 package main
 
 import (
+	"flag"
 	"fmt"
-	"log"
 	"os"
 
 	"neilpa.me/go-jfif"
 )
 
 func main() {
-	f, err := os.Open(os.Args[1])
-	if err != nil {
-		log.Fatal(err)
+	os.Exit(realMain(os.Args[1:]))
+}
+
+func realMain(args []string) int {
+	flag.Usage = printUsage
+	flag.CommandLine.Parse(args)
+
+	if flag.NArg() == 0 {
+		return usageError("no files specified")
 	}
 
-	segs, err := jfif.DecodeSegments(f)
-	if err != nil {
-		log.Fatal(err)
-	}
+	for _, arg := range flag.Args() {
+		f, err := os.Open(arg)
+		if err != nil {
+			return fatal(err.Error())
+		}
 
-	for _, s := range segs {
-		sig, _, _ := s.AppPayload()
-		fmt.Printf("%s\t%d\t%s\n", s.Marker, len(s.Data), sig)
+		segs, err := jfif.DecodeSegments(f)
+		if err != nil {
+			return fatal(err.Error())
+		}
+
+		for _, s := range segs {
+			sig, _, _ := s.AppPayload()
+			fmt.Printf("%s\t%d\t%s\n", s.Marker, len(s.Data), sig)
+		}
 	}
+	return 0
+}
+
+func fatal(format string, args ...interface{}) int {
+	format = os.Args[0] + ": " + format + "\n"
+	fmt.Fprintf(os.Stderr, format, args...)
+	return 1
+}
+
+func usageError(msg string) int {
+	fmt.Fprintln(os.Stderr, msg)
+	printUsage()
+	return 2
+}
+
+func printUsage() {
+	fmt.Fprintf(os.Stderr, `Usage: %s jpeg [jpeg...]
+
+  jfifstat prints segment marker, size, and signature info from JPEG files.
+`, os.Args[0])
+	flag.PrintDefaults()
+	fmt.Fprintln(os.Stderr)
 }
